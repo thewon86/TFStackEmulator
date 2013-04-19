4854021c200f27ab90ccfd442e83894deb850b79
Jan Sandbrink <fischikowski@web.de>
2013-04-19 12:55:04 +0200

    extend Temp-Bricklet

--- TFStackEmulator/Devices/RandomTemperatureBricklet.cs
+++ TFStackEmulator/Devices/RandomTemperatureBricklet.cs
@@ -8,6 +8,10 @@ namespace TFStackEmulator.Devices
 {
     class RandomTemperatureBricklet : EnumeratableDevice
     {
+        private const int GET_TEMPERATURE = 1;
+        private const int SET_TEMPERATURE_CALLBACK_PERIOD = 2;
+        private const int BINDINGS_PING = 128;
+
         public RandomTemperatureBricklet(UID uid)
             : base(uid)
         {
@@ -17,10 +21,20 @@ namespace TFStackEmulator.Devices
         {
             switch (packet.FunctionID)
             {
-                case 1:
+                case GET_TEMPERATURE:
                     packet.Payload = BitConverter.GetBytes((UInt16)2300); //23°C was randomly chosen ;-)
                     return packet;
+                case SET_TEMPERATURE_CALLBACK_PERIOD:
+                    //NOP
+                    if (packet.ResponseExpected)
+                    {
+                        return packet;
+                    }
+                    return null;
+                case BINDINGS_PING:
+                    return null;
                 default:
+                    Console.WriteLine("Unknown function called: {0} (response was {1}expected)", packet.FunctionID, packet.ResponseExpected ? "" : "not ");
                     return null;
             }
         }
